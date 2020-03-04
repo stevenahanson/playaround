12ddfba6da5eb5736940269c6f98ab3166911555
Steven Hanson <steven.hanson@wintechnologies.net>
2020-03-04 14:30:10 +0000

    Add route to different middleware

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('in the middleware');  
-    next(); // to continue
+    res.send('<h1>The "Add product" Page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('in another middleware'); 
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
