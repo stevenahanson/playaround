519f0edc7687c3ea21f770d6aa23a8e4edbde31c
Steven Hanson <steven.hanson@wintechnologies.net>
2020-03-04 16:28:23 +0000

    Add new content to response

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const express = require('express');
 
 const app = express();
 
+// app.use('/', (req, res, next) => {
+//     console.log('This always runs!');  
+//     next();
+// });
+
 app.use('/add-product', (req, res, next) => {
     console.log('in the middleware');  
-    res.send('<h1>The "Add product" Page</h1>');
+    res.send('<form><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 app.use('/', (req, res, next) => {
