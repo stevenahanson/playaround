19d8f3c25b79b209ea50004c690667a7647db263
Steven Hanson <steven.hanson@wintechnologies.net>
2020-03-04 11:25:52 +0000

    Add middleware and next function

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('in the middleware');  
+    next(); // to continue
+});
+
+app.use((req, res, next) => {
+    console.log('in another middleware');  
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
